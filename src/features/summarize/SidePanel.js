1bf0b7311db01e0a487cabc6211653e799146ab1
Linh Vu Thai <vuthailinh.linh7.v7@gmail.com>
2021-12-04 12:14:22 +0700

    Relayout the tool page

--- src/features/summarize/SidePanel.js
+++ src/features/summarize/SidePanel.js
@@ -13,20 +13,17 @@ export default function SidePanel() {
           <a href="/summarize"><Icon name='check square outline'/>Text summarization</a>
         </li>
         <li>
-          <a href="/summarize?random"><Icon name='cube'/>Explore</a>
-        </li>
-        <li>
           <a href="https://arxiv.org/abs/1912.08777" target="blank"><Icon name='file'/> The paper - Pegasus</a>
         </li>
         <li>
           <a href="https://paperswithcode.com/dataset/cnn-daily-mail-1" target="blank"><Icon name='database'/> CNN Dailymail dataset</a>
         </li>
         <li>
-          <a href="#" target="blank"><Icon name='book'/> The notebook</a>
+          <a href="https://colab.research.google.com/drive/1qdvcK8xLUiyEIWtQZ_hkcbG9IpELQ96u?usp=sharing#scrollTo=NF0SLRgee834" target="blank"><Icon name='book'/> The notebook</a>
         </li>
       </ul>
       <div className="memo">
-        Made with: colab, kaggle, python, quart, react, rekit, semantic-ui
+        Made with: colab, python, quart, react, rekit, semantic-ui
       </div>
     </div>
   );
